f860c62c1619d9f2524f7bbcf3d009e0c1883e3d
Szymon Borowiecki <szymboro@gmail.com>
2024-11-14 12:31:44 +0100

    PWA changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,67 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      // includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: "Gunbase",
+        short_name: "Gunbase",
+        description: "Organise your arsenal!",
+        theme_color: "#ff5861",
+        background_color: "#1d232a",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/gunbase.vercel\.app\/.*$/, // Zmodyfikuj wzorzec URL do Twojej aplikacji
+            handler: "NetworkFirst", // Strategia cache'owania
+            options: {
+              cacheName: "api-cache",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // Cache na 30 dni
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   daisyui: {
     themes: ["light", "dark"],
   },
